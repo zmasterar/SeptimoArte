928dbf3d786b14a618240b94da6259db8d5e0924
JM2311 <juan_morales2311@hotmail.com>
2022-05-17 18:21:38 -0300

    Navbar Terminado

--- js/app.js
+++ js/app.js
@@ -4,3 +4,12 @@ let burger = document.querySelector(".botonBurger");
 burger.addEventListener("click", () => {
   burger.classList.toggle("active");
 });
+
+let nav = document.querySelector("nav");
+window.addEventListener("scroll", () => {
+  if (window.pageYOffset > 100) {
+    nav.classList.add("bg-nav", "shadow");
+  } else {
+    nav.classList.remove("bg-nav", "shadow");
+  }
+});
